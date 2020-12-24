02cd8fcbc03ff61121af0a75890d6680234ea7e9
Mehmet <mehmetozturk4705@gmail.com>
2020-12-24 03:21:38 +0200

    Changed release status.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers"
     ],
     packages=["asynchronous_message_handler"],
